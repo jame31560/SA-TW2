294aaaa3ab8c8ca0c4ec44aa5e402654848afb9b
Inky-chew <inky11308@gmail.com>
2019-05-05 08:57:22 +0800

    Update transaction.java

--- transaction.java
+++ transaction.java
@@ -5,10 +5,12 @@ class Controller{
         boolean v = dbmgr.verifyQRCode(payer.qrcode);
             if(v){
                 double a = payee.enterAmount();
+                TransactionHistory trans = new TransactionHistory(payer, payee, a);
 				if(payer.confirmAmount(a)){
 					payer.deductMoney(a);
 					payee.addMoney(a);
 					System.out.println("Transaction success.");
+					trans.printHistory();
 				}else{
 					System.out.println("Balance is not enough.");
 				}
@@ -54,12 +56,29 @@ class QRCode{
 		return isValid;
 	}
 }
+class TransactionHistory{
+    Payer payer;
+    Payee payee;
+    double pay;
+    TransactionHistory(Payer payer, Payee payee, double a){
+        this.payer = payer;
+        this.payee = payee;
+        this.pay = a;
+    }
+    void printHistory(){
+        System.out.println("----TransactionHistory----");
+        System.out.println(payer.getName() + " deduct $" + pay + " ,balance: " + payer.getBalance());
+        System.out.println(payee.getName() + " add $" + pay + " ,balance: " + payer.getBalance());
+    }
+}
 class User{
 	protected String name;
     protected String id;
-    User(String n, String id){
+    protected double balance;
+    User(String n, String id, double b){
         setName(n);
         setId(id);
+        setBalance(b);
     }
     public void setName(String n){
         this.name = n;
@@ -67,21 +86,25 @@ class User{
     public void setId(String id){
         this.id = id;
     }
+    public void setBalance(double b){
+        this.balance = b;
+    }
     public String getName(){
         return name;
     }
     public String getId(){
         return id;
     }
+    public double getBalance(){
+        return balance;
+    }
 }
 class Payer extends User{
-    private double balance;
 	public QRCode qrcode;
 	private boolean isAvailable;
-	Payer(String n, String id, double b){
-		super(n, id);
-		this.balance = b;
-		qrcode = new QRCode(n, "qr01", "abcde");
+	Payer(String n, String id, double b, String qrid, String hash){
+		super(n, id, b);
+		qrcode = new QRCode(n, qrid, hash);
 	}	
 	public boolean confirmAmount(double pay){
 		if(pay < balance){
@@ -97,10 +120,8 @@ class Payer extends User{
 	}
 }
 class Payee extends User{
-	private double balance;
 	Payee(String n, String id, double b){
-		super(n, id);
-		this.balance = b;
+		super(n, id, b);
 	}
 	public double enterAmount(){
 	    Scanner scanner = new Scanner(System.in);
@@ -110,13 +131,13 @@ class Payee extends User{
 	}
 	public void addMoney(double a){
 		balance += a;
-		System.out.println("payer add money: " + a + " balance: " + balance);
+		System.out.println("payee add money: " + a + " balance: " + balance);
 	}
 }
 public class transaction{
     public static void main(String args[]) {
         Controller c = new Controller();
-        Payer payer = new Payer("Amy", "B10623020", 1000);
+        Payer payer = new Payer("Amy", "B10623020", 1000, "qr01", "abcde");
 		Payee payee = new Payee("jay", "B10623019", 1000);
         c.makeTransaction(payer, payee);
     }
