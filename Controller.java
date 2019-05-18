1dae7d84d8c4fc0a304f99545fb7f917370ce38b
JaoJao <jame31560@gmail.com>
2019-05-18 22:30:45 +0800

    Change the List in DBMgr to use CRUD

--- Controller.java
+++ Controller.java
@@ -1,17 +1,22 @@
 import java.io.Console;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import javax.swing.JOptionPane;
 
 public class Controller {
     DBMgr dbMgr = new DBMgr();
     User user;
+    static String userID = null;
     Console console = System.console();
-    
+
     public boolean login(String username, String password) {
         user = dbMgr.getUserByUsername(username);
+        userID = user.getID();
         if (user == null) {
             return false;
         } else {
-            if (password.equals(user.getPassword())) {
+            if (eccrypt(password).equals(user.getPassword())) {
                 return true;
             } else {
                 return false;
@@ -28,58 +33,50 @@ public class Controller {
         return true;
     }
 
-    public String[] regist(String username,
-            String password,
-            String confirmPassword,
-            String name,
-            String phone,
+    public String[] regist(String username, String password, String confirmPassword, String name, String phone,
             int balance) {
-        
-        if (username.equals("") || 
-                dbMgr.getUserByUsername(username) != null) {
-            return new String[] {"Fail",
-                "There has the same username in the system"};
+
+        if (username.equals("") || dbMgr.getUserByUsername(username) != null) {
+            return new String[] { "Fail", "There has the same username in the system" };
         } else if (!password.equals(confirmPassword)) {
-            return new String[] {"Fail",
-                "Password confirm incorrect."};
+            return new String[] { "Fail", "Password confirm incorrect." };
         }
+        System.out.print(eccrypt(password));
         User registUser = new User(username,
-            password,
+            eccrypt(password),
             name,
             balance,
             phone);
         dbMgr.addUser(registUser);
-        return new String[] {"Success"};
+        return new String[] { "Success" };
     }
 
-    public String[] changePassword(String password,
-            String newPassword,
-            String confirmPassword) {
-        if (password.equals(user.getPassword())) {
+    public String[] changePassword(String password, String newPassword, String confirmPassword) {
+        if (eccrypt(password).equals(user.getPassword())) {
             if (newPassword.equals(confirmPassword)) {
-                user.setPassword(newPassword);
-                return new String[] {"Success"};
+                dbMgr.changePassword(userID, eccrypt(newPassword));
+                // user.setPassword(newPassword);
+                return new String[] { "Success" };
             } else {
-                return new String[] {"Fail", "Password confirm incorrect."};
+                return new String[] { "Fail", "Password confirm incorrect." };
             }
         }
-        return new String[] {"Fail", "Password incorrect."};
+        return new String[] { "Fail", "Password incorrect." };
     }
-    
+
     public User getPayer(String QRCodeID) {
         return dbMgr.getUserByQRCodeID(QRCodeID);
     }
-    
+
     public String[] makeTransaction(User payer, int amount) {
         if (amount > payer.getBalance()) {
             return new String[] { "Fail", "Payer's balance is not enough." };
         } else {
             int result = JOptionPane.showConfirmDialog(null,
-               "Is the amount right?",
-               "Confirm",
-               JOptionPane.YES_NO_OPTION,
-               JOptionPane.QUESTION_MESSAGE);
-            
+                "Is the amount right?",
+                "Confirm",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE);
             if (result == JOptionPane.YES_OPTION) {
                 payer.deductMoney(amount);
                 user.addMoney(amount);
@@ -87,6 +84,29 @@ public class Controller {
             } else {
                 return new String[] { "Fail", "Payer reject the amount." };
             }
+        }
+    }
+
+    public String eccrypt(String info){
+        MessageDigest sha;
+        try {
+            sha = MessageDigest.getInstance("SHA-256");
+            byte[] srcBytes = info.getBytes();
+            sha.update(srcBytes);
+            byte[] resultBytes = sha.digest();   
+            char[] hexChar = {  
+                '0', '1', '2', '3', '4', '5', '6', '7',  
+                '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'  
+            };
+            StringBuilder sb = new StringBuilder(resultBytes.length * 2);  
+            for (int i = 0; i < resultBytes.length; i++) {  
+                sb.append(hexChar[(resultBytes[i] & 0xf0) >>> 4]);  
+                sb.append(hexChar[resultBytes[i] & 0x0f]);  
+            }  
+            return sb.toString();  
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
         }
     }
     
--- DBMgr.java
+++ DBMgr.java
@@ -1,37 +1,94 @@
-import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class DBMgr {
-    static List<User> UserList = new ArrayList<User>();
+    Connection conn = null;
+    Statement stmt = null;
+    ResultSet rs = null;
 
     DBMgr () {
-        this.addUser(new User("B10623020", "1234", "Amy1", 1000, "09123456781"));
-        this.addUser(new User("B10623021", "1234", "Amy2", 1000, "09123456782"));
-        this.addUser(new User("B10623022", "1234", "Amy3", 1000, "09123456783"));
-        /*
-        The DBMgr need to add the User from database when it init,
-        But we don't have database now, so I write the test data in here.
-        */
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("connect success to MySQLToJava");
+            conn = DriverManager.getConnection("jdbc:mysql://"
+                + "127.0.0.1:5000/java?"
+                + "user=root&"
+                + "password=root&"
+                + "useUnicode=true");
+            System.out.println("connect success to MySQL");
+            stmt = conn.createStatement();
+        } catch(SQLException excpt) {
+            excpt.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public void addUser(User ob) {
-        UserList.add(ob);
+        try {
+            stmt.executeUpdate("INSERT INTO `userinfo` ("
+                + "`u_name`, `u_username`, `u_password`, "
+                + "`u_phone`, `u_QRcodeID`) VALUES ('"
+                + ob.getName() + "', '"
+                + ob.getID() + "', '"
+                + ob.getPassword() + "', '"
+                + ob.getPhone() + "', '"
+                + ob.getQRCodeID() + "');");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public User getUserByQRCodeID(String QRCodeID) {
-        for (User user : UserList) {
-            if (QRCodeID.equals(user.getQRCodeID())) {
-                return user;
+        try {
+            rs = stmt.executeQuery("SELECT * "
+                + "FROM userinfo WHERE u_QRcodeID = '"
+                + QRCodeID
+                + "';");
+            if(rs.next()){
+                return new User(rs.getString("u_username"),
+                    rs.getString("u_password"),
+                    rs.getString("u_name"),
+                    rs.getInt("u_balance"),
+                    rs.getString("u_phone"));
             }
+            return null;
+        } catch (Exception e) {
+            return null;
         }
-        return null;
     }
 
     public User getUserByUsername(String username) {
-        for (User user : UserList) {
-            if (username.equals(user.getID())) {
-                return user;
+        try {
+            rs = stmt.executeQuery("SELECT * "
+                + "FROM userinfo WHERE u_username = '"
+                + username
+                + "';");
+            if(rs.next()){
+                return new User(rs.getString("u_username"),
+                    rs.getString("u_password"),
+                    rs.getString("u_name"),
+                    rs.getInt("u_balance"),
+                    rs.getString("u_phone"));
             }
+            return null;
+        } catch (Exception e) {
+            return null;
         }
-        return null;
+    }
+
+    public void changePassword(String userID, String password) {
+        try {
+            stmt.executeUpdate("UPDATE `userinfo` SET `u_password` = '"
+                + password + "' "
+                + "WHERE `userinfo`.`u_username` = '"
+                + userID + "';");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
